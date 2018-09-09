e0a394feb5103067a6f75b99b004d866400fbd62
Mikol Faro <mikol.faro@gmail.com>
2018-09-09 23:01:58 +0200

    Add services configuration

---
+++ src/SymfonyApmAgentBundle.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: Mikol Faro <m.faro@engaged.it>
+ * Date: 09/09/2018
+ * Time: 22:45
+ */
+
+namespace MikolFaro\SymfonyApmAgent;
+
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class SymfonyApmAgentBundle extends Bundle
+{
+    
+}
